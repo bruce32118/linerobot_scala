2df2daf4957130fc58acff2f8db217a463cc27d1
bruce32118 <doimustz@gmail.com>
2017-04-17 14:48:27 +0800

    Update pttBeautyCrawler.scala

--- src/main/scala/com/bruce/robotservice/pttBeautyCrawler.scala
+++ src/main/scala/com/bruce/robotservice/pttBeautyCrawler.scala
@@ -4,11 +4,13 @@ import scala.collection.JavaConverters._
 
 object pttBeautyCrawler {
 
-    private val (ptt,pttBeautyAddress) = ("https://www.ptt.cc/","bbs/Beauty/index.html")
+    private lazy val (ptt,pttBeautyAddress) = ("https://www.ptt.cc/","bbs/Beauty/index.html")
     
     def apply() : Map[Int,String] = {
-
+      
+      //用jsoup去解析表特版
       val doc = Jsoup.connect( ptt + pttBeautyAddress ).get
+      //以下就是些用眼睛觀察想撈什麼東西的方法囉
       val elements = doc.select("a.btn.wide").asScala
       var main = "" 
       var beautyURL = Map( 1121 -> "test") 
@@ -19,7 +21,7 @@ object pttBeautyCrawler {
       }
    
       val ptt_num = main.split("index")(1).split("\\.")(0) 
-     
+      //撈三十個頁面
       for(subtraction <- -1 to 30){
           
         val ptt_page = ptt_num.toInt - subtraction
@@ -28,7 +30,7 @@ object pttBeautyCrawler {
         val elementsxs = doc2.select("div.r-ent").asScala 
 
         for(elementsx <- elementsxs) {
-
+         //撈推文爆或是推文大於30的表特文
          if(elementsx.select("span.hl.f1").text != "" || (elementsx.select("span.hl.f3").text != "" && elementsx.select("span.hl.f3").text.toInt > 30)){
                
            val big = ptt + elementsx.select("a").attr("href")
